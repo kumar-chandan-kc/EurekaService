a4350b0b16a5d608c1003cf3f7edbf93a8a1193d
chandank <chandank@azuga.com>
2020-09-14 10:54:52 +0530

    Eureka Service initial rep

---
+++ AzugaEurekaService/src/main/java/com/azuga/reseller/AzugaEurekaService/AzugaEurekaServiceApplication.java
@@ -0,0 +1,13 @@
+package com.azuga.reseller.AzugaEurekaService;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AzugaEurekaServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AzugaEurekaServiceApplication.class, args);
+	}
+
+}
---
+++ AzugaEurekaService/src/test/java/com/azuga/reseller/AzugaEurekaService/AzugaEurekaServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.azuga.reseller.AzugaEurekaService;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AzugaEurekaServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
